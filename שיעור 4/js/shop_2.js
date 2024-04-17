e1f9a096c430f65b80015d53069380f6288ec58b
aviv tal <avivtal357@gmail.com>
2024-04-17 12:26:38 +0300

    ver 0.36.6

--- שיעור 4/js/shop_2.js
+++ שיעור 4/js/shop_2.js
@@ -1,33 +1,33 @@
 function on(value) {
   if(value==1){
-    document.getElementById(overlays[0]).style.display = "block";
+    document.getElementById(overlays[0]).style.visibility = "visible";
   }
   if (value==2){
-    document.getElementById(overlays[1]).style.display = "block";
+    document.getElementById(overlays[1]).style.visibility = "visible";
   }
   if(value == 3 ){
-    document.getElementById(overlays[2]).style.display = "block";
+    document.getElementById(overlays[2]).style.visibility = "visible";
   }
  if(value==4){
-   document.getElementById(overlays[3]).style.display = "block";
+   document.getElementById(overlays[3]).style.visibility = "visible";
  }
  if(value==5){
-  document.getElementById(overlays[4]).style.display = "block";
+  document.getElementById(overlays[4]).style.visibility = "visible";
  }
 if(Value==6){
-  document.getElementById(overlays[5]).style.display = "block";
+  document.getElementById(overlays[5]).style.visibility = "visible";
 }
 
 
 }
   function off() {
     
-    document.getElementById(overlays[0]).style.display = "none";
-    document.getElementById(overlays[1]).style.display = "none";
-    document.getElementById(overlays[2]).style.display = "none";
-    document.getElementById(overlays[3]).style.display = "none";
-    document.getElementById(overlays[4]).style.display = "none";
-    document.getElementById(overlays[5]).style.display = "none";
+    document.getElementById(overlays[0]).style.visibility = "hidden";
+    document.getElementById(overlays[2]).style.visibilityjh = "hidden";
+    document.getElementById(overlays[3]).style.visibilityjh = "hidden";
+    document.getElementById(overlays[5]).style.visibilityjh = "hidden";
+    document.getElementById(overlays[4]).style.visibilityjh = "hidden";
+    document.getElementById(overlays[1]).style.visibilityjh = "hidden";
     
     
   //  document.getElementById(overlays.active).style.display = "none";
@@ -39,14 +39,24 @@ if(Value==6){
  imageList[1] = "./images/nissan qashqai 22.jpg";
 
  function Change_nissan_left(){
-index1 = index1 +1;
-if (index1 == imageList.length){
-  index1 = 0 ;
+index1 = index1 -1;
+if (index1 < 0){
+  index1 = 1 ;
 }
 var images1 = document.getElementById("nis_chg_img");
 images1.src = imageList[index1]
 
 }
+
+function Change_nissan_right(){
+  index1 = index1 +1;
+  if (index1 == imageList.length){
+    index1 = 0 ;
+  }
+  var images1 = document.getElementById("nis_chg_img");
+  images1.src = imageList[index1]
+  
+  }
 
 const overlays = [];
 overlays[0] = "overlay1";
